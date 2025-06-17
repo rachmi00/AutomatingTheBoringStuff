2cb25df817fff0053da91154758856154c78a598
rachmi00 <r.agyingi@gmail.com>
2025-06-17 04:29:41 +0100

    minor change

--- catnapping.py
+++ catnapping.py
@@ -1,5 +1,5 @@
 print('''Dear Alice,
 Eve's cat has been arrested for catnapping, cat burglary, and extortion.
-Sincerely,
+Sincerely
 Bob''')
 print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincerely,\nBob')
